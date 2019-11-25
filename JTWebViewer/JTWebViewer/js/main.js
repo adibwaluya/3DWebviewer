10a80bed9ab52cf2100294f20258976365ee1ed3
s0567271 <adib.waluya@student.htw-berlin.de>
2019-11-26 00:03:17 +0100

    revised

--- JTWebViewer/JTWebViewer/js/main.js
+++ JTWebViewer/JTWebViewer/js/main.js
@@ -1,42 +1,35 @@
 ﻿// Fügen Sie Ihren Code hier ein.
 function loadFile() {
-
     var input, file, fr;
 
     if (typeof window.FileReader !== 'function') {
-
         bodyAppend("p", "The file API isn't supported on this browser yet.");
         return;
     }
 
     input = document.getElementById('fileinput');
-
     if (!input) {
-
         bodyAppend("p", "Um, couldn't find the fileinput element.");
     }
 
     else if (!input.files) {
-
-        bodyAppend("p", "This browser doesn't seem to support the `files` property of file inputs.");
+        bodyAppend("p", "This browser doesn't seem to support the 'files' property of file inputs");
     }
 
     else if (!input.files[0]) {
-
-        bodyAppend("p", "Please select a file before clicking 'Load'");
+        bodyAppend("p", "Please select file before clicking 'Load'");
     }
 
     else {
-
         file = input.files[0];
-        fr = new FileReader();
+        fr = new FileReader;
         fr.onload = receivedText;
         fr.readAsText(file);
     }
 
     function receivedText() {
-
         showResult(fr, "Text");
+
         fr = new FileReader();
         fr.onload = receivedBinary;
         fr.readAsBinaryString(file);
@@ -45,25 +38,35 @@ function loadFile() {
     function receivedBinary() {
         showResult(fr, "Binary");
     }
+
+
 }
 
+function showResult(fr, label) {                        // file reader and label as parameter
+    var markup,
+        result,
+        n,
+        aByte,
+        byteStr,
+        maxbytesVersion;
+    //secondResult,
+    //secondMarkup,
+    //maxbytesFinalFive,
+    //maxbytesByteOrder;
+    //uuid,
+    //identifier;
 
+    markup = [];    // defines an empty object
+    result = fr.result; // result from the file reader
 
-function showResult(fr, label) {
+    // defining the first 32 hexadecimal
+    maxbytesVersion = result.length;
+    if (result.length > 32) maxbytesVersion = 32;
 
-    var markup, result, n, aByte, byteStr, maxbytes, secondResult, byteInt, swap, byteOrder, m;
-
-    markup = [];
-    result = fr.result;
-    maxbytes = result.length;
-
-    if (result.length > 32) maxbytes = 32;
-    for (n = 0; n < maxbytes; ++n) { //result.length; ++n) {
-
+    for (n = 0; n < maxbytesVersion; ++n) {
         aByte = result.charCodeAt(n);
-        byteStr = aByte.toString(16);
+        byteStr = aByte.toString(16);   // radix. Hexadecimal numbers (base 16) specifies the base for representing numeric values
         if (byteStr.length < 2) {
-
             byteStr = "0" + byteStr;
         }
 
@@ -72,55 +75,116 @@ function showResult(fr, label) {
 
     bodyAppend("p", label + " (" + result.length + "  " + result.substr(0, 32) + "):");
     bodyAppend("pre", markup.join(" "));
+    bodyAppend("bo", result.substr(81, 82));
 
     secondResult = fr.secondResult;
-    byteOrder = secondResult.length;
 
-    if (secondResult.length > 81) byteOrder = 81;
-    for (n = 80; n < byteOrder + 1; ++n) {
+    maxbytesByteOrder = secondResult.length;
+    if (secondResult.length > 81) maxbytesByteOrder = 81;
+
+    for (n = 80; n < maxbytesByteOrder; ++n) {
         aByte = secondResult.charCodeAt(n);
-        byteInt = aByte.byteArrayToInt();
-        //byteInt = byteStr.parseInt(32);
-        if (byteInt < 2) {
-
-            byteInt = "0" + byteInt;
+        byteStr = aByte.toString(16);
+        if (byteStr.length < 2) {
+            byteStr = "0" + byteStr;
         }
 
-        markup.push(byteInt);
+        markup.push(byteStr);
+
     }
 
-    bodyAppend("bo", secondResult.substr(80, 82));
-
-    //if (result.length > 81) maxbytes = 81;
-    //for (n = 80; n < maxbytes; ++n) {
-
-    //    aByte = result.charCodeAt(n);
-    //    byteStr = aByte.toString(16);
-    //    if (byteStr.length < 2) {
-    //        byteStr = "0" + byteStr;
-    //    }
-    //    swap = changeEndianness(byteStr);
-
-    //    markup.push(swap);
-    //}
+    bodyAppend("test", secondResult.length + secondResult(81, 82));
 
 
-    //bodyAppend("bo", result.substr(80, 82));
 }
+/* Do we even need this for fstream?
+http = require('http');
+fs = require('fs');
 
-const changeEndianness = (string) => {
-    const result = [];
-    let len = string.length - 2;
-    while (len >= 0) {
-        result.push(string.substr(len, 2));
+http.createServer(function (req, res) {
+    var filename = __dirname + req.url;
+
+    var readStream = fs.createdReadStream(filename);
+
+    readStream.on('error', function (err) {
+        res.end(err);
+    });
+}).listen(8080);
+*/
+
+/*
+ *if (result.length > 145) maxbytesByteOrder = 145;
+
+for (n = 145; maxbytesByteOrder < 145; ++n) {
+    aByte = result.charCodeAt(n);
+    byteStr = aByte.toString(16);
+    if (byteStr.length < 2) {
+        byteStr = "0" + byteStr;
     }
 
-    return result.join('');
+    markup.push(byteStr);
+}
+ * 
+ * */
+
+
+
+
+//bodyAppend("bo", "(" + secondResult.substr(80, 82));
+
+
+
+/* 
+function myFileHeader(fr, label) {
+    var jtVersion,
+        jtByteOrder = 0,
+        jtFileAttributes = 0,
+        jtTOCOffset = 0,
+        generateGUID = createGUID(),
+        fs,
+        myReadStream,
+        readMe,
+        markup,
+        result;
+
+    var http = require('http');
+    var fs = require('fs');
+
+    http.createServer(function (req, res) {
+        var filename = 
+    });
+    result = fr.result;
+    jtVersion = result.length;
+
+   
+
+
+    /*
+     myFileHeader(myReadStream.on('data', function (chunk) {
+
+
+    }));
+     */
+
+
+function createGUID() {
+    var dt = new Date().getTime();
+    var guid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = (dt + Math.random() * 16) % 16 | 0;
+        dt = Math.floor(dt / 16);
+        return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+
+    });
+
+    return guid;
 }
 
+function uuidv4() {
+    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16));
+}
 
 function bodyAppend(tagName, innerHTML) {
-
     var elm;
 
     elm = document.createElement(tagName);
@@ -128,6 +192,7 @@ function bodyAppend(tagName, innerHTML) {
     document.body.appendChild(elm);
 }
 
+/*
 function BitLenghtCodecDecode(nValues, Vecu vCodeText, nBitsCodeText, Veci &ovValues) {
     nBits = 0; //number of codec Bits decoded so far
     nTotalBits = 0; //
@@ -154,3 +219,4 @@ function BitLenghtCodecDecode(nValues, Vecu vCodeText, nBitsCodeText, Veci &ovVa
 
 
 }
+*/
